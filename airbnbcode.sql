9414c41ef738a8f7913cc317a14a1bf4d7c36625
ezra zanger <ezzanger@gmail.com>
2018-07-04 12:30:32 -0400

    remodified airbnb bike drill answers

--- airbnbcode.sql
+++ airbnbcode.sql
@@ -1,17 +1,4 @@
-SELECT
-MinTemperatureF, *
-FROM
-weather 
-WHERE 
-ZIP = 94301  AND
-EVENTS = 'Rain'
-
-SELECT 
-*
-FROM 
-stations
-WHERE 
-station_id = 84
+The ID's and durations for all trips of duration greater than 500, ordered by duration.
 
 SELECT 
 duration,
@@ -22,18 +9,39 @@ WHERE
 duration > '500'  
 ORDER BY duration
 
+
+Every column of the stations table for station id 84.
+
+SELECT 
+*
+FROM 
+stations
+WHERE 
+station_id = 84
+
+
+
+The min temperatures of all the occurrences of rain in zip 94301.
+
+SELECT
+MinTemperatureF, *
+FROM
+weather 
+WHERE 
+ZIP = 94301  AND
+EVENTS = 'Rain'
+
+
+What was the hottest day in our data set? Where was that?
+
 SELECT
 MaxTemperatureF, Date, Zip
 FROM
 weather 
 ORDER BY MaxTemperatureF desc
 
-SELECT 
-start_station
-FROM 
-trips
-WHERE 
-start_date like '2015-11-17'    **had trouble
+
+How many trips started at each station?
 
 SELECT
 dockcount
@@ -41,12 +49,15 @@ FROM
 stations
 ORDER BY station_id, desc
 
+What's the shortest trip that happened?
 
 SELECT 
-duration, start_station
+duration, start_station, trip_id
 FROM 
 trips
 ORDER BY duration asc
+
+What is the average trip duration, by end station?
 
 SELECT 
 AVG(duration) duration, 
@@ -56,15 +67,7 @@ trips
 GROUP by end_station
 
 
-shortest trip
-SELECT
-duration, trip_id
-FROM 
-trips
-GROUP by duration
-
-
-airbnb
+Airbnb Drill
 
 1. most expensive listings : Centrum West
 SELECT 
